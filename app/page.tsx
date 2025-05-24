3f6724c3545719511a863cb7d81e427b95a92d60
callmeniyu <getniyashere@gmail.com>
2025-05-24 18:22:22 +0530

    responsiveness for larger device

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ const [toast, setToast] = useState<{
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className="relative overflow-x-hidden px-4 md:px-5 lg:px-16 sm:px-8 md:bg-transparent"
+            className="max-w-screen-lg relative overflow-x-hidden px-4 md:px-5 lg:px-16 sm:px-8 md:bg-transparent"
         >
             <Navbar scrollToNewsletter={handleNewsletterClick} />
             <Hero setToast={setToast} emailInputRef={emailInputRef} />
