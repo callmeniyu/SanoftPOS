27ada7ae147a736af2fa0a786f1487aeec88fb07
callmeniyu <getniyashere@gmail.com>
2025-06-19 15:49:39 +0530

    add banner

--- components/InvoiceBanner.tsx
+++ components/InvoiceBanner.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 const InvoiceBanner = () => {
     return (
-        <section className="flex gap-5 justify-center bg-primary h-96 relative overflow-hidden border-y-2 banner-pattern-left">
+        <section className="flex my-8 gap-5 justify-center bg-primary h-96 relative overflow-hidden border-y-2 banner-pattern-left">
             {/* <Image src="/images/banner_shape.png" alt="banner-shape" width={360} height={360} className="absolute right-0" /> */}
             <div className="w-1/2 flex justify-center items-center">
                 <h2 className="xs:text-2xl md:text-4xl font-bold font-gilroy text-white text-center">Malaysia E-Invoice Ready!</h2>
